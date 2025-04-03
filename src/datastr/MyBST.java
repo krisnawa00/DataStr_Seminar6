c58d9a7b5356495ad92f38508a3d145570d70507
EmreOren <m.emreoren@gmail.com>
2025-04-03 15:22:52 +0300

    Kaut kas jauns

--- src/datastr/MyBST.java
+++ src/datastr/MyBST.java
@@ -4,20 +4,24 @@ public class MyBST<Ttype> {
 
 	private MyNode<Ttype> root = null;
 	private int counter = 0;
-
-	// neveidošu beazrgumenta konstruktoru, jo tas ir jau no Object klases pieejams
-
-	public boolean isFull()// koks būs pilns, ja RAM atmiņā nebūs vietas
+	
+	//neveidošu beazrgumenta konstruktoru, jo tas ir jau no Object klases pieejams
+	
+	
+	public boolean isFull()//koks būs pilns, ja RAM atmiņā nebūs vietas 
 	{
-		try {
+		try
+		{
 			MyNode<Integer> newNode = new MyNode<Integer>(4);
 			return false;
-
-		} catch (OutOfMemoryError e) {
+			
+		}
+		catch (OutOfMemoryError e) {
 			return true;
 		}
 	}
-
+	
+	
 	public boolean isEmpty() {
 		return (counter == 0);
 	}
@@ -25,63 +29,95 @@ public class MyBST<Ttype> {
 	public int howManyElements() {
 		return counter;
 	}
-
-	public void add(Ttype element) throws Exception {
-
-		if (element == null) {
-			throw new Exception("Padotais elem nedrikst but ka null");
+	
+	public void add(Ttype element) throws Exception
+	{
+		if(element == null)
+		{
+			throw new Exception("Padotais elements nedrīkst būt kā null");
 		}
-
-		if (isFull()) {
+		
+		if(isFull())
+		{
 			throw new Exception("BST ir pilns un nevar pievienot jaunu elementu");
 		}
-
-		if (isEmpty()) {
+		
+		if(isEmpty())//ja ir jāievieto pirmais elements
+		{
 			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
 			root = newNode;
 			counter++;
-		} else// nav labi
+		}
+		else//ja ir jāievieto pārejie elementi
 		{
-
 			addHelper(root, element);
 			counter++;
-
 		}
-	}
-
-	private void addHelper(MyNode<Ttype> currentNode, Ttype newElement) {
-		if( ((Comparable)newElement).compareTo(currentNode.getElement()) == 1) {
 		
-			if(currentNode.getRightChildNode() == null) {
+	}
+	
+	private void addHelper(MyNode<Ttype>  currentNode, Ttype newElement)
+	{
+		//ja jaunais elements ir lielāks par esošo mezglu, tad jaunais elements būs labajā puse
+		if(  ((Comparable)newElement).compareTo(currentNode.getElement()) == 1   )
+		{
+			//ja nekas nav labajā pusē, tad pievienojam jauno mezglu ka labo bērnu
+			if(currentNode.getRightChildNode() == null)
+			{
 				
-				MyNode <Ttype> newNode = new MyNode <Ttype> (newElement);
+				MyNode<Ttype> newNode = new MyNode<Ttype>(newElement);
 				
-			
 				currentNode.setRightChildNode(newNode);
 				newNode.setParent(currentNode);
-						
-			}	
-			else 
+			}
+			else //labajā pusē nav vietas, tāpēc izsauc funkciju rekursīvi, bet uz labo bērnu
 			{
 				addHelper(currentNode.getRightChildNode(), newElement);
 			}
-			
 		}
-		else if ( ((Comparable)newElement).compareTo(currentNode.getElement()) == -1) {
-			if(currentNode.getLeftChildNode() == null) {
-				
-				MyNode <Ttype> newNode = new MyNode <Ttype> (newElement);
-				
-			
-				currentNode.setLeftChildNode(newNode);
-				newNode.setParent(currentNode);
-						
-			}	
-			else 
-			{
-				addHelper(currentNode.getLeftChildNode(), newElement);
-			}
+		//ja jaunais elements ir mazaāks par esošo mezglu, tad jaunais elements būs kreisajā puse
+ 		else if(  ((Comparable)newElement).compareTo(currentNode.getElement()) == -1   )
+ 		{
+ 			//ja nekas nav kreisajā pusē, tad pievienojam jauno mezglu kā kreiso bērnu
+ 			if(currentNode.getLeftChildNode() == null)
+ 			{
+ 				MyNode<Ttype> newNode = new MyNode<Ttype>(newElement);
+ 				currentNode.setLeftChildNode(newNode);
+ 				newNode.setParent(currentNode);
+ 			}
+ 			else  //kreisajā pusē nav vietas, tāpēc izsauc funkciju rekursīvi, bet uz kreiso bērnu
+ 			{
+ 				addHelper(currentNode.getLeftChildNode(), newElement);
+ 			}
+ 				
+ 		}
+	}
+	
+	public void print() throws Exception{
+		if(isEmpty()) {
+			throw new Exception("Nav ko printet");
+		}
+		printHelp(root);
+	}
+	
+	private void printHelp(MyNode <Ttype> currentNode) {
+		
+		System.out.println(currentNode.getElement());
+		
+		if(currentNode.getLeftChildNode() != null ) {
+			System.out.println("Kreisais berns: " 
+					+ currentNode.getLeftChildNode().getElement()
+					+ "(" + currentNode.getElement() + ")");
+			printHelp (currentNode.getLeftChildNode());
+		}
+		
+		if(currentNode.getRightChildNode() !=null) {
+			System.out.println("Labais berns: " 
+					+ currentNode.getRightChildNode().getElement()
+					+ "(" + currentNode.getElement() + ")");
+			printHelp (currentNode.getRightChildNode());
 			
 		}
 	}
+	
 }
